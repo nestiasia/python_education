b861b6d3663d06e239309aa5fa14afcd04c908d6
naska <>
2026-01-28 22:34:23 +0300

    2urok

---
+++ 2.py
@@ -0,0 +1,96 @@
+from datetime import datetime
+import random
+
+def if_conditions():
+    a = 10
+    b = 10
+    c = "www"
+    x = a == b
+
+    print(type(x), x)
+
+    # условные операторы
+    x = input('введи число: ')
+
+    x = int(x)
+    if x < 10 and x > 0:
+        print ("1")
+    elif x > 9 and x < 100:
+        print ("2")
+    elif x >= 100 and x < 1000:
+        print ("3")
+    else:
+        print ("хуйня брат")
+
+def main():
+    """
+    list
+    if elif else
+
+    for range while + while True
+    break continue
+    """
+    
+    # if_conditions()
+
+    # if not x.isnumeric():
+    #     print("введи блин число!")
+
+    # x = int(x)
+
+    # x > 0 -> "{x} положительный"
+    # x < 0 -> "{x} отрицательный"
+
+    # if x > 0:
+    #     print(f"{x} - положительный")
+    # elif x < 0:
+    #     print(f"{x} - отрицательный")
+    # elif x == 0:
+    #     print('x == 0')
+    # else:
+    #     print('вообще не знаю такое')
+
+    # циклы и листы
+    l = [66, 9, -3, 457, -346, 99, 22, -1235, 55, 54]
+
+    # print(l)
+
+    # print(l[0], l[1], l[2], l[3], l[4], l[5])
+    x = 0
+
+    for number in l:
+        if number % 2 == 0:
+            x = x + 1
+
+        if x == 3:
+            # print(number)
+            break # тут произойдет выход из цикла
+
+    # for n in range(5, 9):
+    #     print(n)
+
+    # цикл while
+
+    # for num in l:
+    #     print(num)
+
+    # while True:
+    #     now = datetime.now()
+        
+    #     if now.hour == 22 and now.minute == 25:
+    #         print('hello')
+    #         break
+    
+    """
+    домашка:
+
+    игра угадай число
+
+    + под * если 10 попыток комп говрит ты проиграл и выдат ответ
+    """
+
+    random_number = random.randint(1, 10)
+    # print(random_number)
+
+main()
+
