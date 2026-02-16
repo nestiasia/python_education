2b5b06d4d5fea6f809d06ff0731cee6dc7a81e88
naska <>
2026-02-16 19:27:59 +0300

    3 dz

--- trenirovka.py
+++ trenirovka.py
@@ -14,19 +14,59 @@ def main():
     # print (f"тем кому {age} вход запрещен")
     # игра угадай число 
     
-    a = random.randint(1, 10)
-    print (a)
-    while True:
-        c = input ("Введи число: ")
-        c = int(c)
-        if c > a:
-            print (f"Число {c} больше загаданного")
+    # a = random.randint(1, 10)
+    # print (a)
+    # x = 0
+    # while x < 10:
+    #     c = input ("Введи число: ")
+    #     c = int(c)
+    #     if c > a:
+    #         x = x + 1
+    #         print (f"Число {c} больше загаданного")
         
-        elif c < a:
-             print (f"Число {c} меньше загаданного")
-        else:
-            print (f"Ты выиграл, ответ - {a}")
-            break
-        
+    #     elif c < a:
+    #          x = x + 1
+    #          print (f"Число {c} меньше загаданного")
+    #     else:
+    #         print (f"Ты выиграл, ответ - {a}")
+    #         break
+    # else:
+    #     print (f"Ты проиграл, ответ - {a}")
 
+    # for i in range(1, 6):
+    #         for j in range(1, 6):
+    #             for k in range (1, 6):
+    #                 print(i, j, k)
+    matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+    
+    
+    for index in range (0, len(matrix)):
+        # print (index)
+        a = matrix [index]
+        # print (a)
+        for dindex in range (0, len(a)):
+            # print (dindex)
+            b = a[dindex]
+            print (b)
+    # print (matrix [0])
+    # while True:
+    #     a = input ("Какая строка у загаданной позиции? - ")
+    #     a = int(a)
+    #     a = a-1
+    #     b = matrix [a]
+      
+    #     c = input ("а столбец? - ")
+    #     c = int(c)
+    #     c = c - 1 
+    #     d = b[c]
+    #     print (f'Твое число {d}')
+    #     break
+
+
+
+   
 main()    
